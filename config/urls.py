50dc655232d7478f79092aad2c630cda2920b3fa
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-02-12 23:12:55 +0500

    urls.py

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls'), name='app')
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
